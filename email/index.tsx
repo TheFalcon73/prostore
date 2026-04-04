1025acabce8061b796a0c9a0b158a5f9766995de
TheFalcon73 <ffalconz@gmail.com>
2026-04-04 13:45:35 -0600

    Almost complete. Resend bug fix

--- email/index.tsx
+++ email/index.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-import { Resend } from 'resend';
+
+
+/* import { Resend } from 'resend';
 import { SENDER_EMAIL, APP_NAME } from '@/lib/constants';
 import { Order } from '@/types';
 require('dotenv').config();
@@ -16,4 +18,19 @@ export const sendPurchaseReceipt = async ({order}:{order: Order}) => {
         react: <PurchaseReceiptEmail order={order}/>
     })
 
-}
+} */
+import { Resend } from 'resend';
+import { SENDER_EMAIL, APP_NAME } from '@/lib/constants';
+import { Order } from '@/types';
+import PurchaseReceiptEmail from './purchase-receipt';
+
+export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
+  const resend = new Resend(process.env.RESEND_API_KEY as string);
+
+  await resend.emails.send({
+    from: `${APP_NAME} <${SENDER_EMAIL}>`,
+    to: order.user.email,
+    subject: `Order Confirmation ${order.id}`,
+    react: <PurchaseReceiptEmail order={order} />
+  });
+};
